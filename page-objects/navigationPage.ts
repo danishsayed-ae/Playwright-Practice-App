ab0b9db59b03d59568268af23f77c123a7750b16
Danish Sayed <danishsayed.ae@gmail.com>
2024-05-26 11:56:20 +0400

    Update navigationPage.ts

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,13 +1,12 @@
 import { Page } from "@playwright/test";
 
 class NavigationPage{
-    readonly page : Page
+    readonly page: Page
     constructor(page: Page){
         this.page = page
     }
 
-    async formsLayoutPage() {
+    async formLayoutsPage(){
         await this.page.getByText('Forms').click()
-        await this.page.getByText('Form Layouts').click()
     }
 }
