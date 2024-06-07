756718bafcf5075ec705b66d93067d59b4b8d38b
Danish Sayed <danishsayed.ae@gmail.com>
2024-06-07 17:31:38 +0400

    Added faker library

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test} from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager';
+import {fa, faker} from '@faker-js/faker'
 
 test.beforeEach('Navigating to the web page', async ({page}) => {
     await page.goto('http://localhost:4200/')
@@ -16,7 +17,9 @@ test('Navigate to Forms page', async ({page}) => {
 
 test('Parameterised Method', async ({page}) => {
     const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(100)}@test.com`
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormUsingCredentialsAndSelectOption('test@test.com', 'Test@1234', 'Option 1')
-    await pm.onFormLayoutsPage().submitInlineFormUsingCredentialsAndCheckbox('Danish Sayed', 'ds@test.com', false)
+    await pm.onFormLayoutsPage().submitInlineFormUsingCredentialsAndCheckbox(randomFullName, randomEmail, false)
 });
