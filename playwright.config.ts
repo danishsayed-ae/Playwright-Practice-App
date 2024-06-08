c6d8647f1d44e6c1f40349c9508d315ec36918fc
Danish Sayed <danishsayed.ae@gmail.com>
2024-06-08 14:18:28 +0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,13 +34,13 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    
+
     // actionTimeout: 5000,
     navigationTimeout: 10000,
     geolocation: { longitude: 41.890221, latitude: 12.492348 },
     permissions: ['geolocation'],
 
-    // This is used to capture video
+    // This is used to capture video only when we run the test from CLI
     video: 'on'
   },
 
